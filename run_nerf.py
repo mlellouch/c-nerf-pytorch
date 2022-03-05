8e678beda010630f14c66703dfbcf146442ebd97
Michael Lellouch <mlellouch@aligntech.com>
2022-03-05 05:49:10 +0200

    code supports cpu

--- run_nerf.py
+++ run_nerf.py
@@ -873,6 +873,8 @@ def train():
 
 
 if __name__=='__main__':
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
     train()
